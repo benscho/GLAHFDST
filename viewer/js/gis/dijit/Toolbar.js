1292e72712e38ff04dfe1344cd83a656b22b4d06
benscho <ben.schoenfeldt@gmail.com>
2015-05-12 16:28:22 -0400

    5/12 Updates
    Toolbar development is continuing, but not as rapidly as one might hope.
    Goal is to borrow functionality from measure, identify, and find.

--- viewer/js/gis/dijit/Toolbar.js
+++ viewer/js/gis/dijit/Toolbar.js
@@ -12,10 +12,17 @@ define([
 	
 	'dstore/Memory',
 	
+	'esri/dijit/Measurement',
+	'esri/units',
+	
+	'dojo/dom-construct',
+	'dojo/request',
+	'dojo/html',
 	'dojo/_base/lang',
 	'dojo/topic',
-	'dojo/text!./Toolbar/Templates/Toolbar.html'
-], function (declare, _WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin, Button, ComboButton, Menu, MenuItem, Select, TextBox, Memory, lang, topic, ToolbarTemplate) {
+	'dojo/text!./Toolbar/Templates/Toolbar.html',
+], function (declare, _WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin, Button, ComboButton, Menu, MenuItem, Select,
+			 TextBox, Memory, Measurement, units, domConstruct, request, html, lang, topic, ToolbarTemplate) {
 	return declare([_WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin], {
 		widgetsInTemplate: true,
         templateString: ToolbarTemplate,
@@ -24,94 +31,45 @@ define([
 		},
 		startMeasure: function () {
 			this.clearContents();
-			var areaButton = new Button({
-				label: "Area",
-				onClick: function() {
-				
-				}
-			}).placeAt(dojo.byId("toolbarContents")).startup();
-			var distanceButton = new Button({
-				label: "Distance",
-				onClick: function() {
-				
-				}
-			}).placeAt(dojo.byId("toolbarContents")).startup();
-			var locationButton = new Button({
-				label: "Location",
-				onClick: function() {
-				
-				}
-			}).placeAt(dojo.byId("toolbarContents")).startup();
-			var unitsMenu = new Menu({ style: "display: none;"});
-			var miles = new MenuItem({
-				label: "Miles",
-				onClick: function(){
-				
-				}
+			/*this.measure = new Measurement({
+                map: this.map,
+                defaultAreaUnit: units.SQUARE_MILES,
+                defaultLengthUnit: units.MILES
+            }, domConstruct.create('div'));
+            this.measure.startup();*/
+			request.get("/js/gis/dijit/Toolbar/Templates/Measure.html").then(function(results) {
+				html.set(dojo.byId("toolbarContents"), results);
 			});
-			var kilometers = new MenuItem({
-				label: "Kilometers",
-				onClick: function(){
-				
-				}
-			});
-			unitsMenu.addChild(miles);
-			unitsMenu.addChild(kilometers);
-			var unitsButton = new ComboButton({
-				label: "Units",
-				dropDown: unitsMenu
-			}).placeAt(dojo.byId("toolbarContents")).startup();
 		},
+		/*measureArea: function () {
+			this.measure.setTool("area", true);
+		},
+		measureDistance: function () {
+			this.measure.setTool("distance", true);
+		},
+		measureLocation: function () {
+			this.measure.setTool("location", true);
+		},*/
 		startIdentify: function () {
 			this.clearContents();
-			dojo.byId("toolbarContents").innerHTML = "Choose \"All Visible Layers\" or a single layer for identify:";
-			/*var identifyStore = new Memory({
-				data: [
-					{
-						label:"*** All Visible Layers ***", 
-						id: "all"
-					}
-				]
-			});*/
-			var identifySelect = new Select({
-				name: "identifySelect",
-				//store: identifyStore,
-				options:[
-					{ value: "", label:"*** All Visible Layers ***", selected: true }
-				],
-				labelAttr: "label"
-			}).placeAt(dojo.byId("toolbarContents")).startup();
+			request.get("/js/gis/dijit/Toolbar/Templates/Identify.html").then(function(results){
+				html.set(dojo.byId("toolbarContents"), results);
+			});
 		},
 		startFind: function () {
 			this.clearContents();
-			dojo.byId("toolbarContents").innerHTML = "<b>Select query:</b>";
-			/*var findStore = new Memory({
-				data: [
-					{
-						label: "Find a Basin by name",
-						id: "basin-name"
-					}
-				]
-			});*/
-			var findSelect = new Select({
-				name: "findSelect",
-				//store: findStore,
-				options:[
-					{ value: "", label: "Find a Basin by name", selected: true }
-				],
-				labelAttr: "label"
-			}).placeAt(dojo.byId("toolbarContents")).startup();
-			dojo.byId("toolbarContents").innerHTML += "<b>Search for:</b>";
-			var findText = new TextBox({
-				name: "findText",
-				value: ""
-			}).placeAt(dojo.byId("toolbarContents")).startup();
+			request.get("/js/gis/dijit/Toolbar/Templates/Find.html").then(function(results){
+				html.set(dojo.byId("toolbarContents"), results);
+			});
 		},
 		startPrint: function () {
 			this.clearContents();
 		},
 		clearContents: function () {
 			dojo.byId("toolbarContents").innerHTML = "";
+			if(this.measure){
+				this.measure.destroy();
+			}
 		},
 		setMapClickMode: function () {
 			
